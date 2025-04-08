5c2d4722f718f6263157a5f65fe8751da4a3a4ed
Robert Velnic <robert.velnic@optimizely.com>
2025-04-08 12:47:30 +0200

    Use HTML img tag instead of Next.Image

--- src/components/partials/autosized-image/autosized-image.tsx
+++ src/components/partials/autosized-image/autosized-image.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React, { useState } from "react";
 
 export type AutosizedImageProps = {
@@ -7,27 +6,11 @@ export type AutosizedImageProps = {
 };
 
 export const AutosizedImage: React.FC<AutosizedImageProps> = ({ src, alt = "" }) => {
-  const [imageSize, setSmageSize] = useState({
-    width: 1000,
-    height: 1000,
-  });
-
   return (
-    <Image
+    <img
       src={src}
       alt={alt}
-      priority={true}
-      onLoad={(evt) => {
-        const target = evt.target as HTMLImageElement;
-        setSmageSize({
-          width: target.naturalWidth,
-          height: target.naturalHeight,
-        });
-      }}
-      width={imageSize.width}
-      height={imageSize.height}
       className={"object-contain w-full h-full"}
-      quality={100}
     />
   );
 };
